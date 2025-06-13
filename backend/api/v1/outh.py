be2d44e9a0d67f028878f2ec7a77429734329cd4
Lobosanplay <parra.santiago2008@gmail.com>
2025-06-13 12:21:34 -0500

    feacture: create enpoint login and register

---
+++ backend/api/v1/outh.py
@@ -0,0 +1,35 @@
+from fastapi import APiRouter, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from typing import List
+from models.user  import UserRegister
+from depends.outh_depends import get_user, get_password_hash
+from db.mongo import users_collection
+
+router = APIRouter()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+@router.post("/register")
+async def register(user: UseUserRegister):
+    existing_user = await get_user(user.email)
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    hashed_password = get_password_hash(user.password)
+    user_dict = {
+        "mail": user.email,
+        "hashed_password": hashed_password
+    }
+    await users_collection.insert_one(user_dict)
+    return {"msg": "Successfully registered user"}
+
+@app.post("/login", response_model=Token)
+async def login(user: UserLogin):
+    db_user = await get_user(user.email)
+    if not db_user:
+        raise HTTPException(status_code=400, detail="Incorrect credentials")
+    if not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=400, detail="Incorrect credentials")
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": db_user.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
---
+++ backend/main.py
@@ -0,0 +1,21 @@
+from doctest import debug
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="ClearMeet",
+    version="0.1.0",
+    debug=True,
+    description="ClearMeet API",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(
+    prefix="/api",
+)
